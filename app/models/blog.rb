a8ac4b7cd2fa24ac6af10898f3e90cc53aadb90e
Karanveer Singh <singhkaranveer671@gmail.com>
2021-02-09 12:36:47 -0800

    toggle status using enum added in view, route and toggle status method in controller

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+  #set enum and column want to use as enum and then key value pairs that can be part of enum draft is default value and published is 1
+  # this way other people will only see published blogs not draft ones
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
