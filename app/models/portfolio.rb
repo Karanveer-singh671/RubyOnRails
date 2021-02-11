71b222f3bca08da4ce088f145867eb0d84759a16
Karanveer Singh <singhkaranveer671@gmail.com>
2021-02-11 10:01:18 -0800

    set defaults in model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,4 +5,14 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Ruby on Rails')
   end
   scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
+
+  #set callback to change defaults e.g go to form 
+  after_initialize :set_defaults
+
+
+  def set_defaults
+    # for the portfolios main image 
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x300"
+  end
 end
