5f6382be79a977057912fe4490ff657d2e7820b2
Karanveer Singh <singhkaranveer671@gmail.com>
2021-02-12 09:58:34 -0800

    added mult technologies nested in model portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,9 @@
 class Portfolio < ApplicationRecord
   # rails recogonizes ies for plural of technology
   has_many :technologies
+  # -> or lambda same thing to send these need to write use name_attributes where name is  accepts_nested_attributes_for  :name 
+  # needs to be a array of nested hashes (objects)
+  accepts_nested_attributes_for :technologies, reject_if: ->(attributes){ attributes['name'].blank? }
   includes Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -16,6 +19,7 @@ class Portfolio < ApplicationRecord
   def set_defaults
     # for the portfolios main image 
     self.main_image ||= Placeholder.image_generator(height: '400', width: '600')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '300')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '300')
   end
 end
+
